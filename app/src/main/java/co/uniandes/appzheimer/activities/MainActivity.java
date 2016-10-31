43f6e5163fd4a33951b80d0dc5affa5ee1c1a8cd
sabisambra <s.abisambra125@uniandes.edu.co>
2016-10-30 22:57:53 -0500

    Avances Persistencia

--- app/src/main/java/co/uniandes/appzheimer/activities/MainActivity.java
+++ app/src/main/java/co/uniandes/appzheimer/activities/MainActivity.java
@@ -8,8 +8,16 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import co.uniandes.appzheimer.R;
 import co.uniandes.appzheimer.source.AppZheimer;
+import co.uniandes.appzheimer.source.Paciente;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,13 +25,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if(false){
-            Button btnCrearPerfil = (Button) findViewById(R.id.crearPerfilBtn);
-            btnCrearPerfil.setVisibility(View.INVISIBLE);
 
-        }else{
-            Button btnMostrarMenu = (Button) findViewById(R.id.ingresarBtn);
-            btnMostrarMenu.setVisibility(View.INVISIBLE);
+        try {
+            InputStream archivo = openFileInput(CrearPerfilActivity.DATOSUSUARIO);
+            if (archivo!=null)
+            {
+                InputStreamReader temp = new InputStreamReader(archivo);
+                BufferedReader lector = new BufferedReader(temp);
+                String linea = lector.readLine();
+                if (!linea.equals("No hay sesion"))
+                {
+                    String nombre = linea;
+                    String apodo = lector.readLine();
+                    String fecha = lector.readLine();
+                    SimpleDateFormat df = new SimpleDateFormat("dd/mm/yyyy");
+                    Date fecha2 = df.parse(fecha);
+                    AppZheimer.darInstancia().setPaciente(new Paciente(nombre,apodo,fecha2));
+                    Intent intent = new Intent(this,MenuPrincipalActivity.class);
+                    startActivity(intent);
+                }
+                lector.close();
+                temp.close();
+                archivo.close();
+            }
+        }
+        catch (Exception e){
+            if(false){
+                Button btnCrearPerfil = (Button) findViewById(R.id.crearPerfilBtn);
+                btnCrearPerfil.setVisibility(View.INVISIBLE);
+
+            }else{
+                Button btnMostrarMenu = (Button) findViewById(R.id.ingresarBtn);
+                btnMostrarMenu.setVisibility(View.INVISIBLE);
+            }
         }
     }
 
---
+++ app/src/main/java/co/uniandes/appzheimer/source/DBHelper.java
@@ -0,0 +1,47 @@
+package co.uniandes.appzheimer.source;
+
+/**
+ * Created by Santiago on 10/30/2016.
+ */
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBHelper extends SQLiteOpenHelper
+{
+    /**
+     * Constante que muestra la version de la base de datos
+     */
+    private static final int DATA_BASE_VERSION = 1;
+
+    /**
+     * El nombre de la base de datos
+     */
+    private static  final String DATA_BASE_NAME = "AppZheimer.db";
+
+    /**
+     * Metodo constructor de la base de datos
+     * @param context
+     */
+    public DBHelper(Context context)
+    {
+        super(context,DATA_BASE_NAME,null,DATA_BASE_VERSION);
+    }
+
+    public void onCreate(SQLiteDatabase db)
+    {
+        String createTableFamiliares = "CREATE TABLE FAMILIARES (nombrealias TEXT PRIMARY KEY, nombre TEXT, apodo TEXT, relacion TEXT)";
+        String createTableEventos = "CREATE TABLE EVENTOS (nombre TEXT PRIMARY KEY, hora TEXT, conPariente BIT)";
+        db.execSQL(createTableFamiliares);
+        db.execSQL(createTableEventos);
+    }
+
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
+    {
+        db.execSQL("DROP TABLE IF EXIST FAMILIARES");
+        db.execSQL("DROP TABLE IF EXIST EVENTOS");
+        onCreate(db);
+    }
+}
+
